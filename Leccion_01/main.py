ec11895d03d296bc2000888e408bc13df6882392
Franmy Reynoso <freynoso23@gmail.com>
2020-09-07 13:37:04 -0400

    Listo!

---
+++ Leccion_01/main.py
@@ -0,0 +1,23 @@
+# Eso es un comentario
+
+# Hacer salida de datos estandar
+print('Hello')
+print(2 + 4 * 6)
+
+# Entrada de datos
+edad = input("Escriba su edad:")
+edadSumana = (int(edad) + 1)
+print("La edad es:", edadSumana)
+
+# Almacenamiento en memoria de datos
+numero = 110
+print(numero)
+print(numero + 5) 
+
+# Expresiones
+dollar = 1
+peso = 58.59
+
+tasaCambio = input('Ingrese Cantidad de dollares:')
+print('Conversion:', int(tasaCambio) * peso)
+
