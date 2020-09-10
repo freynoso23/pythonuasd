3d72c5834368b56d66cb9e6e0ffe1e662a6d2a95
Franmy Reynoso <freynoso23@gmail.com>
2020-09-10 10:56:50 -0400

    Funciones

---
+++ Leccion_2/ejemplo2.3.py
@@ -0,0 +1,23 @@
+######### Funcion #########
+# Crear una funcion saludar.
+def saludar():
+    print("Hola a todos!")
+
+#Invocar funcion.
+saludar()
+
+# Parametros
+
+def saludarAlguien (nombre):
+    print("Hola " + nombre)
+
+
+saludarAlguien('Franmy')
+saludarAlguien("Juan Jose")
+
+
+def duplicar(x):
+    return(x * 2)
+
+
+print(duplicar(130))
